470e55801476479cbac39a41e0519f413552812f
hupe13 <hupe13@t-online.de>
2022-01-26 13:14:53 +0100

    readme

--- leafext-dsgvo.php
+++ leafext-dsgvo.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://github.com/hupe13/extensions-leaflet-map-dsgvo
  * GitHub Plugin URI: https://github.com/hupe13/extensions-leaflet-map-dsgvo
  * Primary Branch: main
- * Version:     220123
+ * Version:     220125
  * Author:      hupe13
 **/
 
